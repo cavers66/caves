647a97ca26c26e6b7fcd6e0c9890a639e98327e4
Ralf Rötzer <from-github@cavers.de>
2013-01-30 22:22:51 +0100

    Testweise Models erstellt
    Funktionsweise muß mir nur noch klar werden :)

---
+++ system/modules/caves/models/CaveArchiveModel.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2013 Ralf Rötzer
+ * 
+ * @package cave
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Run in a custom namespace, so the class can be replaced
+ */
+namespace Caves;
+
+
+/**
+ * Reads and writes cave archives
+ * 
+ * @package   Models
+ * @author    Ralf Rötzer
+ * @copyright 2013 Ralf Rötzer
+ */
+class CaveArchiveModel extends \Model
+{
+
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_cave_archive';
+
+
+	/**
+	 * Find multiple cave archives by their IDs
+	 * 
+	 * @param array $arrIds     An array of archive IDs
+	 * @param array $arrOptions An optional options array
+	 * 
+	 * @return \Model\Collection|null A collection of models or null if there are no news archives
+	 */
+	public static function findMultipleByIds($arrIds, array $arrOptions=array())
+	{
+		if (!is_array($arrIds) || empty($arrIds))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order'] = \Database::getInstance()->findInSet("$t.id", $arrIds);
+		}
+
+		return static::findBy(array("$t.id IN(" . implode(',', array_map('intval', $arrIds)) . ")"), null, $arrOptions);
+	}
+}
---
+++ system/modules/caves/models/CaveModel.php
@@ -0,0 +1,269 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2013 Ralf Rötzer
+ * 
+ * @package cave
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Run in a custom namespace, so the class can be replaced
+ */
+namespace Caves;
+
+
+/**
+ * Reads and writes caves
+ * 
+ * @package   Models
+ * @author    Ralf Rötzer
+ * @copyright 2013 Ralf Rötzer
+ */
+class CaveModel extends \Model
+{
+
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_cave';
+
+
+	/**
+	 * Find published cave items by their parent ID and ID or alias
+	 * 
+	 * @param mixed $varId      The numeric ID or alias name
+	 * @param array $arrPids    An array of parent IDs
+	 * @param array $arrOptions An optional options array
+	 * 
+	 * @return \Model|null The CaveModel or null if there are no news
+	 */
+	
+    public static function findByAlias($varId)
+    {
+    $t = static::$strTable;
+    return static::findBy(array($t.".alias = ? "), array($varId));
+        
+    }    
+    
+    
+    
+    
+    public static function findPublishedByParentAndIdOrAlias($varId, $arrPids, array $arrOptions=array())
+	{
+		if (!is_array($arrPids) || empty($arrPids))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("($t.id=? OR $t.alias=?) AND $t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		return static::findBy($arrColumns, array((is_numeric($varId) ? $varId : 0), $varId), $arrOptions);
+	}
+
+
+	/**
+	 * Find published caves items by their parent ID
+	 * 
+	 * @param array   $arrPids     An array of news archive IDs
+	 * @param integer $intLimit    An optional limit
+	 * @param integer $intOffset   An optional offset
+	 * @param array   $arrOptions  An optional options array
+	 * 
+	 * @return \Model\Collection|null A collection of models or null if there are no news
+	 */
+	public static function findPublishedByPids($arrPids, $intLimit=0, $intOffset=0, array $arrOptions=array())
+	{
+		if (!is_array($arrPids) || empty($arrPids))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("$t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+
+		// Never return unpublished elements in the back end, so they don't end up in the RSS feed
+		if (!BE_USER_LOGGED_IN || TL_MODE == 'BE')
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order']  = "$t.date DESC";
+		}
+
+		$arrOptions['limit']  = $intLimit;
+		$arrOptions['offset'] = $intOffset;
+
+		return static::findBy($arrColumns, null, $arrOptions);
+	}
+
+
+	/**
+	 * Count published cave items by their parent ID
+	 * 
+	 * @param array   $arrPids     An array of cave archive IDs
+	 * @param array   $arrOptions  An optional options array
+	 * 
+	 * @return integer The number of news items
+	 */
+	public static function countPublishedByPids($arrPids, $blnFeatured=null, array $arrOptions=array())
+	{
+		if (!is_array($arrPids) || empty($arrPids))
+		{
+			return 0;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("$t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		return static::countBy($arrColumns, null, $arrOptions);
+	}
+
+
+	/**
+	 * Find published cave items with the default redirect target by their parent ID
+	 * 
+	 * @param integer $intPid     The cave archive ID
+	 * @param array   $arrOptions An optional options array
+	 * 
+	 * @return \Model\Collection|null A collection of models or null if there are no news
+	 */
+	public static function findPublishedDefaultByPid($intPid, array $arrOptions=array())
+	{
+		$t = static::$strTable;
+		$arrColumns = array("$t.pid=? AND $t.source='default'");
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order'] = "$t.date DESC";
+		}
+
+		return static::findBy($arrColumns, $intPid, $arrOptions);
+	}
+
+
+	/**
+	 * Find published cave items by their parent ID
+	 * 
+	 * @param integer $intId      The cave archive ID
+	 * @param integer $intLimit   An optional limit
+	 * @param array   $arrOptions An optional options array
+	 * 
+	 * @return \Model\Collection|null A collection of models or null if there are no news
+	 */
+	public static function findPublishedByPid($intId, $intLimit=0, array $arrOptions=array())
+	{
+		$time = time();
+		$t = static::$strTable;
+
+		$arrColumns = array("$t.pid=? AND ($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1");
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order'] = "$t.date DESC";
+		}
+
+		if ($intLimit > 0)
+		{
+			$arrOptions['limit'] = $intLimit;
+		}
+
+		return static::findBy($arrColumns, $intId, $arrOptions);
+	}
+
+
+	/**
+	 * Find all published cave items of a certain period of time by their parent ID
+	 * 
+	 * @param integer $intFrom    The start date as Unix timestamp
+	 * @param integer $intTo      The end date as Unix timestamp
+	 * @param array   $arrPids    An array of news archive IDs
+	 * @param integer $intLimit   An optional limit
+	 * @param integer $intOffset  An optional offset
+	 * @param array   $arrOptions An optional options array
+	 * 
+	 * @return \Model\Collection|null A collection of models or null if there are no news
+	 */
+	public static function findPublishedFromToByPids($intFrom, $intTo, $arrPids, $intLimit=0, $intOffset=0, array $arrOptions=array())
+	{
+		if (!is_array($arrPids) || empty($arrPids))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("$t.date>=? AND $t.date<=? AND $t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order']  = "$t.date DESC";
+		}
+
+		$arrOptions['limit']  = $intLimit;
+		$arrOptions['offset'] = $intOffset;
+
+		return static::findBy($arrColumns, array($intFrom, $intTo), $arrOptions);
+	}
+
+
+	/**
+	 * Count all published cave items of a certain period of time by their parent ID
+	 * 
+	 * @param integer $intFrom    The start date as Unix timestamp
+	 * @param integer $intTo      The end date as Unix timestamp
+	 * @param array   $arrPids    An array of news archive IDs
+	 * @param array   $arrOptions An optional options array
+	 * 
+	 * @return integer The number of news items
+	 */
+	public static function countPublishedFromToByPids($intFrom, $intTo, $arrPids, array $arrOptions=array())
+	{
+		if (!is_array($arrPids) || empty($arrPids))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("$t.date>=? AND $t.date<=? AND $t.pid IN(" . implode(',', array_map('intval', $arrPids)) . ")");
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		return static::countBy($arrColumns, array($intFrom, $intTo), $arrOptions);
+	}
+}
