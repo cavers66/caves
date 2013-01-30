e97c4135ac33215f2d0135271357736809951c80
Ralf Rötzer <from-github@cavers.de>
2013-01-30 22:18:11 +0100

    Eigene Übersetzungen für Frontendausgabe

--- system/modules/caves/languages/de/default.php
+++ system/modules/caves/languages/de/default.php
@@ -15,4 +15,33 @@
 /**
  * Miscellaneous
  */
-$GLOBALS['TL_LANG']['MSC'][''] = '';
+$GLOBALS['TL_LANG']['CAVE']['cadastrenumber']   = 'Katasternummer:';
+$GLOBALS['TL_LANG']['CAVE']['map']              = 'Kartenblatt:';
+$GLOBALS['TL_LANG']['CAVE']['city']             = 'PLZ / Ort:';
+$GLOBALS['TL_LANG']['CAVE']['street']           = 'Straße:';
+$GLOBALS['TL_LANG']['CAVE']['coordinates']      = 'Koordinaten:';
+$GLOBALS['TL_LANG']['CAVE']['altitude']         = 'Höhe über NN:';
+$GLOBALS['TL_LANG']['CAVE']['directions#c#']    = 'Wegbeschreibung:';
+$GLOBALS['TL_LANG']['CAVE']['category']         = 'Kategorie:';
+$GLOBALS['TL_LANG']['CAVE']['mainlength']       = 'Befahrbare Länge:';
+$GLOBALS['TL_LANG']['CAVE']['totallength']      = 'Gesamtlänge:';
+$GLOBALS['TL_LANG']['CAVE']['islock#c#']        = 'Wegen Fledermausschutz geschlossen:';
+$GLOBALS['TL_LANG']['CAVE']['notpublicopen#c#'] = 'Höhle ist verschlossen:';
+$GLOBALS['TL_LANG']['CAVE']['contact']          ='Kontakt für Anfragen:';
+$GLOBALS['TL_LANG']['CAVE']['evaluation']       ='Bewertung:';
+$GLOBALS['TL_LANG']['CAVE']['difficulty']       ='Schwierigkeit:';
+$GLOBALS['TL_LANG']['CAVE']['wheelinglife']     ='Befahrungsdauer:';
+$GLOBALS['TL_LANG']['CAVE']['equipment']        ='Ausrüstung:';
+$GLOBALS['TL_LANG']['CAVE']['cadastre_legend']  ='Katasterdaten';
+$GLOBALS['TL_LANG']['CAVE']['address_legend']   ='Adressen & Koordinaten'; 
+$GLOBALS['TL_LANG']['CAVE']['data_legend']      ='Höhlendaten'; 
+$GLOBALS['TL_LANG']['CAVE']['access_legend']    ='Fledermausschutz & Zugang'; 
+$GLOBALS['TL_LANG']['CAVE']['statistic_legend'] ='Bewertung & Zeit'; 
+$GLOBALS['TL_LANG']['CAVE']['equipment_legend'] ='Ausrüstung'; 
+$GLOBALS['TL_LANG']['CAVE']['links_legend']     ='Links';
+
+$GLOBALS['TL_LANG']['CAVE']['meter']            ='Meter';
+$GLOBALS['TL_LANG']['CAVE']['from']             ='vom';
+$GLOBALS['TL_LANG']['CAVE']['to']               ='bis';
+$GLOBALS['TL_LANG']['CAVE']['hours']            ='Stunden';
+  
